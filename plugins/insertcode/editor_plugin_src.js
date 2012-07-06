7fd422815cd859becc3779c192e99ed142da667e
Kolier.Li <kolier.li@gmail.com>
2012-07-07 03:21:52 +0800

    Format editor_plugin_src.js

--- plugins/insertcode/editor_plugin_src.js
+++ plugins/insertcode/editor_plugin_src.js
@@ -1,47 +1,50 @@
 (function() {
-	tinymce.create('tinymce.plugins.InsertCode', {
 
-		init : function(ed, url) {
-			ed.addCommand('mceInsertCode', function() {
-				ed.windowManager.open({
-					file : url + '/dialog.htm',
-					width : 480,
-					height : 320,
-					inline : 1
-				}, {
-					plugin_url : url, // Plugin absolute URL
-					some_custom_arg : 'custom arg' // Custom argument
-				});
-			});
+tinymce.create('tinymce.plugins.InsertCode', {
 
-			// Register example button
-			ed.addButton('insertcode', {
-			  title: 'Insert Code',
-				cmd : 'mceInsertCode',
-				image : url + '/img/code.png'
-			});
+  init : function(ed, url) {
+    ed.addCommand('mceInsertCode', function() {
+      ed.windowManager.open({
+        file : url + '/dialog.htm',
+        width : 480,
+        height : 320,
+        inline : 1
+      }, {
+        plugin_url : url, // Plugin absolute URL
+        some_custom_arg : 'custom arg' // Custom argument
+      });
+    });
 
-			// Add a node change handler, selects the button in the UI when a image is selected
-			ed.onNodeChange.add(function(ed, cm, n) {
-			    cm.setActive('insertcode', n.nodeName == 'PRE');
-			});
-		},
+    // Register example button
+    ed.addButton('insertcode', {
+      title : 'Insert Code',
+      cmd : 'mceInsertCode',
+      image : url + '/img/code.png'
+    });
 
-		createControl : function(n, cm) {
-			return null;
-		},
+    // Add a node change handler, selects the button in the UI when a image is
+    // selected
+    ed.onNodeChange.add(function(ed, cm, n) {
+      cm.setActive('insertcode', n.nodeName == 'PRE');
+    });
+  },
 
-		getInfo : function() {
-			return {
-				longname : 'Insert Code for SyntaxHighlighter',
-				author : 'Kolier Li',
-				authorurl : 'http://kolier.li',
-				infourl: '',
-				version : "1.0"
-			};
-		}
-	});
+  createControl : function(n, cm) {
+    return null;
+  },
 
-	// Register plugin
-	tinymce.PluginManager.add('insertcode', tinymce.plugins.InsertCode);
+  getInfo : function() {
+    return {
+      longname : 'Insert Code for SyntaxHighlighter',
+      author : 'Kolier Li',
+      authorurl : 'http://kolier.li',
+      infourl : '',
+      version : "1.0"
+    };
+  }
+});
+
+// Register plugin
+tinymce.PluginManager.add('insertcode', tinymce.plugins.InsertCode);
+
 })();
